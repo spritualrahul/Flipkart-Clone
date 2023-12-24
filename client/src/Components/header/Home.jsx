8971694ee386bac14fb39d4fc3cacefdd8488b09
spritualrahul <98818584+spritualrahul@users.noreply.github.com>
2023-12-24 13:02:40 +0530

    carousel successfully added

--- client/src/Components/header/Home.jsx
+++ client/src/Components/header/Home.jsx
@@ -1,11 +1,24 @@
+import { Fragment } from 'react';
+
 import Navbar from './Navbar';
 import Banner from './Home/Banner';
+import {Box, styled}from '@mui/material'
+
+
+const Component = styled(Box)`
+     padding:10px 10px;
+     background:#f2f2f2;
+`
+
 export default function Home() {
   return (
-    <div>
+    <>
        <Navbar/>
+       <Component>
        <Banner/>
-    </div>
+       </Component>
+       
+    </>
   
   )
 }
