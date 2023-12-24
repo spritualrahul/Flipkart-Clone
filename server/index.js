e4b85c4739bf936aa37d31bd33bb208c9df4c446
spritualrahul <98818584+spritualrahul@users.noreply.github.com>
2023-12-24 21:41:05 +0530

    server added

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+
+const app = express();
+
+
+
+const PORT = 8000;
+
+
+app.listen(PORT, ()=>console.log(`server is runing successfully on port ${PORT}`)) // to make server using listen function
+
